70c87c86970400e55c30e21c30cf9303156a1d79
Pun <pratik.pun@sky.uk>
2023-10-18 16:17:35 +0100

    updated highscore to to be recorded in data.txt and added snake.reset() after collisions

--- Day20/main.py
+++ Day20/main.py
@@ -42,9 +42,11 @@ while game_is_on:
         or snake.head.ycor() < -280
     ):
         scoreboard.reset()
+        snake.reset()
 
     # Detect collision with tail
     for segment in snake.segments[1:]:
         if snake.head.distance(segment) < 10:
             scoreboard.reset()
+            snake.reset()
 screen.exitonclick()
--- Day20/scoreboard.py
+++ Day20/scoreboard.py
@@ -8,7 +8,8 @@ class Scoreboard(Turtle):
     def __init__(self):
         super().__init__()
         self.score = 0
-        self.highscore = 0
+        with open("data.txt") as data:
+            self.highscore = int(data.read())
         self.color("white")
         self.penup()
         self.goto(0, 260)
@@ -30,5 +31,7 @@ class Scoreboard(Turtle):
     def reset(self):
         if self.score > self.highscore:
             self.highscore = self.score
+            with open("data.txt", mode="w") as data:
+                data.write(f"{self.highscore}")
         self.score = 0
         self.update_scorebord()
